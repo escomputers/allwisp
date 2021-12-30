d35cfdf8ef5335e6313467a16fd83daa7bde39ec
Iuris39 <95967839+Iuris39@users.noreply.github.com>
2021-12-30 16:05:47 +0100

    Update and rename nuovodocumento.py to calcolo_giorni_usati.py

--- allwisp/calcolo_giorni_usati.py
+++ allwisp/calcolo_giorni_usati.py
@@ -10,7 +10,7 @@ datainiziosenzaora=dataoggetto.date()
 n=1
 ultimadata=dataoggetto+relativedelta(months=n)
 dataaddattamento=ultimadata.replace(day=9)#questo considera fino al giorno 9, il 10 inizia la fatturazione
-datafinalecon9=dataaddattamento.date()
+datafinalecon9=dataaddattamento.date()umento.py
 nuovadatafinale=ultimadata.replace(day=10) #da questa data inizia la fatturazione
 datafinale=ultimadata.date()
 datafinalecon10=nuovadatafinale.date()
@@ -21,18 +21,14 @@ datafinalecon10=nuovadatafinale.date()
  
 def numOfDays(datainiziosenzaora, datafinalecon9):
     return (datafinalecon9-datainiziosenzaora).days
-     
-#print(numOfDays(datainiziosenzaora, datafinalecon10))
 
 
-differenzagiorni=numOfDays(datainiziosenzaora, datafinalecon9)
+differenzagiorni=numOfDays(datainiziosenzaora, datafinalecprint('prezzo finale', a)on9)
 print(differenzagiorni)
-b=15. #prendere il valore dell'offerta)
+prezzo=15. #prendere il valore dell'offerta)
 
-tariffa_addattamento=b/30
-a=tariffa_addattamento*differenzagiorni
+tariffa_addattamento=prezzo/30*differenzagiorni
 
-print('prezzo finale', a)
 
 '''se presente la spunta nel html prova(boolean=true) inizia la funzione di addattamento dopo 14 giorni
 se presente la spunta non adattare inizia la fatturazione direttamente il 10 del mese successivo,
