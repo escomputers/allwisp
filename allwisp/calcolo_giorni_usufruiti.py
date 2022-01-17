55c7d795cef95d877d164c3e6269d14c55bce3f7
escomputers <emilianos13@gmail.com>
2022-01-17 18:28:21 +0100

    typo error removed

--- allwisp/calcolo_giorni_usufruiti.py
+++ allwisp/calcolo_giorni_usufruiti.py
@@ -10,7 +10,7 @@ datainiziosenzaora=dataoggetto.date()
 n=1
 ultimadata=dataoggetto+relativedelta(months=n)
 dataaddattamento=ultimadata.replace(day=9)#questo considera fino al giorno 9, il 10 inizia la fatturazione
-datafinalecon9=dataaddattamento.date()umento.py
+datafinalecon9=dataaddattamento.date()
 nuovadatafinale=ultimadata.replace(day=10) #da questa data inizia la fatturazione
 datafinale=ultimadata.date()
 datafinalecon10=nuovadatafinale.date()
